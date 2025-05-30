741d259932933e0972c9762c62348f6b44c64112
KeikoMollerecho <keicocco.s@gmail.com>
2025-05-30 11:09:29 +0200

    update github link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ export const Footer = () => {
                <a href="https://www.linkedin.com/in/keiko-m%C3%B6ller-2bba7a2bb/"><img src="/img/linkedin2.png" alt="linkedin" className='w-[90px] hover:scale-110 transition' /></a>
             </div>
             <div className='p-2'>
-               <a href="github.com/Keikomllrhttps"><img src="/img/github2.png" alt="github" className='w-[90px] hover:scale-110 transition'/></a>
+               <a href="https://www.github.com/Keikomllr/"><img src="/img/github2.png" alt="github" className='w-[90px] hover:scale-110 transition'/></a>
             </div>
             <div className='p-2'>
                <a href="https://www.instagram.com/keicoccosuzuki/"><img src="/img/instagram.png" alt="instagram" className='w-[85px] hover:scale-110 transition'/></a>
